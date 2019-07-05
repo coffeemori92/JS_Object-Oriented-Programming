b5ae32748f5bce1034423e93d485f768c0cd9830
coffeemori92 <coffeemori92@naver.com>
2019-07-05 16:04:49 +0900

    Addition Object.create()

--- prototype_inheritance.js
+++ prototype_inheritance.js
@@ -1,6 +1,8 @@
 let superObj = {superVal : 'super'};
-let subObj   = {subVal : 'sub'};
-subObj.__proto__ = superObj;
+// let subObj   = {subVal : 'sub'};
+// subObj.__proto__ = superObj;
+let subObj = Object.create(superObj);
+subObj.subVal = 'sub';
 console.log('subObj.subVal', subObj.subVal);
 console.log('subObj.superVal', subObj.superVal);
 subObj.superVal = 'sub';

b2aab05f0cc91b638b9f95c64d521e26f94c7a74
coffeemori92 <coffeemori92@naver.com>
2019-07-05 16:21:30 +0900

    __proto__ or Object.create()

--- prototype_inheritance.js
+++ prototype_inheritance.js
@@ -7,3 +7,23 @@ console.log('subObj.subVal', subObj.subVal);
 console.log('subObj.superVal', subObj.superVal);
 subObj.superVal = 'sub';
 console.log('superObj.superVal', superObj.superVal);
+
+let kim = {
+    name : 'kim',
+    first : 10,
+    second : 20,
+    sum : function(){
+        return this.first + this.second;
+    }
+}
+let lee = {
+    name : 'lee',
+    first : 10,
+    second : 10,
+    avg : function(){
+        return (this.first + this.second) / 2;
+    }
+}
+lee.__proto__ = kim;
+console.log("lee.sum() : ", lee.sum());
+console.log("lee.avg() : ", lee.avg());

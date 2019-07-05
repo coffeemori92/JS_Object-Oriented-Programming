a7fba0931ba5ba2c94f2d373cd37e44d97730885
coffeemori92 <coffeemori92@naver.com>
2019-07-05 14:12:02 +0900

    Addition prototype.js

---
+++ prototype.js
@@ -0,0 +1,21 @@
+function Person(name, first, second, third) {
+    this.name = name,
+    this.first = first,
+    this.second = second,
+    this.third = third
+}
+
+Person.prototype.sum = function() {
+    return 'prototype : ' + (this.first + this.second);
+}
+
+let kim = new Person('kim', 10, 20, 30);
+kim.sum = function() {
+    return 'this : ' + (this.first + this.second);
+}
+let lee = new Person('lee', 10, 10, 10);
+// lee.sum = function() {
+//     return 'modified : ' + (this.first + this.second);
+// }
+console.log("kim.sum()", kim.sum());
+console.log("lee.sum()", lee.sum());
